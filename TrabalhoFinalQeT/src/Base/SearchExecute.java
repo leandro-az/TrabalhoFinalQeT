cc8aa6ef5a14c1ac39bc64e48cf394d8ba85e920
Lucas Thimoteo <Lucas Thimoteo@DESKTOP-U1PPBCE>
2017-12-02 19:08:13 -0200

    Added some Logging on SearchExecute

--- TrabalhoFinalQeT/src/Base/SearchExecute.java
+++ TrabalhoFinalQeT/src/Base/SearchExecute.java
@@ -2,27 +2,40 @@ package Base;
 
 
 import static Base.ResultType.SearchResult.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public final class SearchExecute {
     
-    public static ResultType result ;
+    private Logger logger = Logger.getLogger(SearchExecute.class.getName());
+    public ResultType result ;
     
-    public static ResultType search(int[] array, int key,SearchType type, int modelOrdenation) {
+    public ResultType search(int[] array, int key,SearchType type, int modelOrdenation) {
+        
+        logger.setLevel(Level.OFF);
+        
+        logger.info("SEARCHING:"+key+" TYPE:"+ type);
         result = new ResultType(NotFound, null, null);
         switch(type){
             case LessThan:
+               logger.info("LESSTHAN");
                searchLessThan(array,key,modelOrdenation);
+               logger.info("RESULT:"+result.toString());
                break;
             case LessThanEquals:
+                logger.info("LESSTHANEQUALS");
                 searchLessThanEquals(array,key,modelOrdenation); 
                 break;
             case Equals:
+                logger.info("EQUALS");
                 searchEquals(array,key);
                 break;
             case GreaterThanEquals:
+                logger.info("GREATERTHANEQUALS");
                searchGreaterThanEquals(array,key,modelOrdenation);
                break;
             case GreaterThan:
+                logger.info("GREATER");
                 searchGreaterThan(array,key,modelOrdenation); 
                 break;
                 
@@ -30,7 +43,7 @@ public final class SearchExecute {
         return result;
     }
 
-    private static void searchLessThan(int[] array, int key, int modelOrdenation) {
+    private void searchLessThan(int[] array, int key, int modelOrdenation) {
                
           for (int i = array.length-1 ; i==0 ; i--) {
             if(array[i]<= key){
@@ -45,7 +58,7 @@ public final class SearchExecute {
             
     }
 
-    private static void searchLessThanEquals(int[] array, int key, int modelOrdenation) {
+    private void searchLessThanEquals(int[] array, int key, int modelOrdenation) {
           for (int i = array.length-1 ; i==0 ; i--) {
             if(array[i]<=key){
               result.setSearchResult(FoundLess);
@@ -64,7 +77,7 @@ public final class SearchExecute {
     
     }
 
-    private static void searchEquals(int[] array, int key) {
+    private void searchEquals(int[] array, int key) {
          
          for (int i = array.length-1 ; i==0 ; i--) {
             if(array[i]== key){
@@ -76,7 +89,7 @@ public final class SearchExecute {
         }
     }
     
-     private static void searchGreaterThan(int[] array, int key, int modelOrdenation) {
+     private void searchGreaterThan(int[] array, int key, int modelOrdenation) {
        for (int i =0; i== array.length-1 ; i++) {
             if(array[i]>= key){
               result.setSearchResult(FoundGreater);
@@ -89,7 +102,7 @@ public final class SearchExecute {
           }
     }
 
-    private static void searchGreaterThanEquals(int[] array, int key, int modelOrdenation) {
+    private void searchGreaterThanEquals(int[] array, int key, int modelOrdenation) {
        for (int i =0; i== array.length-1 ; i++) {
             if(array[i]>= key){
               result.setSearchResult(FoundGreater);
