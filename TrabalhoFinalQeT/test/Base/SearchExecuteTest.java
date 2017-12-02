bdd98523bfc7f6cad472a7a48fba48615093754d
Lucas Thimoteo <Lucas Thimoteo@DESKTOP-U1PPBCE>
2017-12-02 14:17:44 -0200

    Added 5 tests (Crescent and LessThan)

--- TrabalhoFinalQeT/test/Base/SearchExecuteTest.java
+++ TrabalhoFinalQeT/test/Base/SearchExecuteTest.java
@@ -1,6 +1,5 @@
 package Base;
 
-import junit.framework.Assert;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -11,7 +10,12 @@ import static org.junit.Assert.*;
 public class SearchExecuteTest {
     
     private int[] getCres(){
-        int[] array = {1,2,3,4}; 
+        int[] array = {5,10,15,20,25}; 
+        return array;
+    }
+    
+    private int[] getDecr(){
+        int[] array = {25,20,15,10,5}; 
         return array;
     }
    
@@ -20,7 +24,48 @@ public class SearchExecuteTest {
         
         ResultType result = SearchExecute.search(getCres(),0,SearchType.LessThan);
         
-        Assert.assertEquals(0, result.getResultIndex().intValue());
+        assertEquals(-1, result.getResultIndex());
+        assertEquals(-1, result.getResultValue());
+        assertEquals(ResultType.SearchResult.NotFound,result.getSearchResult());
     }
     
+    @Test
+    public void cres_less_left() {
+        
+        ResultType result = SearchExecute.search(getCres(),5,SearchType.LessThan);
+        
+        assertEquals(-1, result.getResultIndex());
+        assertEquals(-1, result.getResultValue());
+        assertEquals(ResultType.SearchResult.NotFound,result.getSearchResult());
+    }
+    
+    @Test
+    public void cres_less_center() {
+        
+        ResultType result = SearchExecute.search(getCres(),15,SearchType.LessThan);
+        
+        assertEquals(1, result.getResultIndex());
+        assertEquals(10, result.getResultValue());
+        assertEquals(ResultType.SearchResult.FoundLess,result.getSearchResult());
+    }
+    
+    @Test
+    public void cres_less_right() {
+        
+        ResultType result = SearchExecute.search(getCres(),25,SearchType.LessThan);
+        
+        assertEquals(3, result.getResultIndex());
+        assertEquals(20, result.getResultValue());
+        assertEquals(ResultType.SearchResult.FoundLess,result.getSearchResult());
+    }
+    
+    @Test
+    public void cres_less_farright() {
+        
+        ResultType result = SearchExecute.search(getCres(),30,SearchType.LessThan);
+        
+        assertEquals(4, result.getResultIndex());
+        assertEquals(25, result.getResultValue());
+        assertEquals(ResultType.SearchResult.FoundLess,result.getSearchResult());
+    }
 }
