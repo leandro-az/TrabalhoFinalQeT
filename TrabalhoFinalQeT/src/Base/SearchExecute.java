9f476a247106605e1fbe5075c7e15e0dc1065a13
Lucas Thimoteo <Lucas Thimoteo@DESKTOP-U1PPBCE>
2017-12-02 23:21:22 -0200

    Decreased ciclomatic complexity to 13

--- TrabalhoFinalQeT/src/Base/SearchExecute.java
+++ TrabalhoFinalQeT/src/Base/SearchExecute.java
@@ -1,125 +1,68 @@
 package Base;
 
-
 import static Base.ResultType.SearchResult.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import static Base.SearchType.*;
 
 public final class SearchExecute {
-    
-    private Logger logger = Logger.getLogger(SearchExecute.class.getName());
-    public ResultType result ;
-    
-    public ResultType search(int[] array, int key,SearchType type, int modelOrdenation) {
-        
-        logger.setLevel(Level.OFF);
-        
-        logger.info("SEARCHING:"+key+" TYPE:"+ type);
-        result = new ResultType(NotFound, null, null);
-        switch(type){
-            case LessThan:
-               logger.info("LESSTHAN");
-               searchLessThan(array,key,modelOrdenation);
-               logger.info("RESULT:"+result.toString());
-               break;
-            case LessThanEquals:
-                logger.info("LESSTHANEQUALS");
-                searchLessThanEquals(array,key,modelOrdenation); 
-                break;
-            case Equals:
-                logger.info("EQUALS");
-                searchEquals(array,key);
-                break;
-            case GreaterThanEquals:
-                logger.info("GREATERTHANEQUALS");
-               searchGreaterThanEquals(array,key,modelOrdenation);
-               break;
-            case GreaterThan:
-                logger.info("GREATER");
-                searchGreaterThan(array,key,modelOrdenation); 
-                break;
-                
+
+    public ResultType search(int[] array, int key, SearchType type, int order) {
+
+        ResultType result = new ResultType(NotFound, null, null);
+
+        for (int i = (array.length - 1) * (1 - order);
+                i * (order) + (-1 * (1 - order)) < array.length * (order) + i * (1 - order); //
+                i = i + (2 * order) - 1) {
+
+            switch (type) {
+                case LessThan:
+                    if (array[i] < key) {
+                        result.setSearchResult(FoundLess);
+                        result.setResultIndex(i);
+                        result.setResultValue(array[i]);
+                    }
+                    break;
+                case LessThanEquals:
+                    if (array[i] < key) {
+                        result.setSearchResult(FoundLess);
+                        result.setResultIndex(i);
+                        result.setResultValue(array[i]);
+                    } else if (array[i] == key) {
+                        result.setSearchResult(FoundExact);
+                        result.setResultIndex(i);
+                        result.setResultValue(array[i]);
+                    }
+                    break;
+                case Equals:
+                    if (key == array[i]) {
+                        result.setSearchResult(FoundExact);
+                        result.setResultIndex(i);
+                        result.setResultValue(array[i]);
+                        return result;
+                    }
+                    break;
+                case GreaterThanEquals:
+                    if (array[i] > key) {
+                        result.setSearchResult(FoundGreater);
+                        result.setResultIndex(i);
+                        result.setResultValue(array[i]);
+                        return result;
+                    } else if (array[i] == key) {
+                        result.setSearchResult(FoundExact);
+                        result.setResultIndex(i);
+                        result.setResultValue(array[i]);
+                        return result;
+                    }
+                    break;
+                case GreaterThan:
+                    if (array[i] > key) {
+                        result.setSearchResult(FoundGreater);
+                        result.setResultIndex(i);
+                        result.setResultValue(array[i]);
+                        return result;
+                    }
+                    break;
+            }
         }
         return result;
     }
-
-    private void searchLessThan(int[] array, int key, int modelOrdenation) {
-               
-          for (int i = array.length-1 ; i>=0 ; i--) {
-            if(array[i]< key){
-              result.setSearchResult(FoundLess);
-              result.setResultIndex(i);
-              result.setResultValue(array[i]); 
-              if(modelOrdenation==1)
-                        return;
-            }
-            
-          }
-            
-    }
-
-    private void searchLessThanEquals(int[] array, int key, int modelOrdenation) {
-          for (int i = array.length-1 ; i>=0 ; i--) {
-            if(array[i]<=key){
-              result.setSearchResult(FoundLess);
-              result.setResultIndex(i);
-              result.setResultValue(array[i]);
-              if(array[i]==key){
-                  result.setSearchResult(FoundExact);
-                  return;
-              }
-              if(modelOrdenation==1)
-                        return;       
-              
-            }
-            
-          }
-    
-    }
-
-    private void searchEquals(int[] array, int key) {
-         
-         for (int i = array.length-1 ; i>=0 ; i--) {
-            if(array[i]== key){
-                System.out.println(">>>>"+ key);
-              result.setSearchResult(FoundExact);
-              result.setResultIndex(i);
-              result.setResultValue(array[i]);               
-            }
-            
-        }
-    }
-    
-     private void searchGreaterThan(int[] array, int key, int modelOrdenation) {
-       for (int i =0; i<= array.length-1 ; i++) {
-            if(array[i]> key){
-              result.setSearchResult(FoundGreater);
-              result.setResultIndex(i);
-              result.setResultValue(array[i]);
-               if(modelOrdenation==1)
-                        return; 
-            }
-            
-          }
-    }
-
-    private void searchGreaterThanEquals(int[] array, int key, int modelOrdenation) {
-       for (int i =0; i<= array.length-1 ; i++) {
-            if(array[i]>= key){
-              result.setSearchResult(FoundGreater);
-              result.setResultIndex(i);
-              result.setResultValue(array[i]);
-              if(array[i]==key){
-                  result.setSearchResult(FoundExact);
-                  return;
-              }
-               if(modelOrdenation==1)
-                        return; 
-            }
-            
-          }
-    }
-
-   
-    
 }
